ea6a1ddbb675373b372d81bcf1236f1292d83a4d
AdhamKhaled1510 <your-adham509k@gmail.com>
2026-09-03 17:58:33 +0300

    fix: daily report items table note about base prices before adjustments

--- src/js/daily-report.js
+++ src/js/daily-report.js
@@ -232,6 +232,7 @@ async function showDayReport() {
           (pendingInvoices.length ? '<div class="card"><span>\u0641\u0648\u0627\u062a\u064a\u0631 \u0645\u0639\u0644\u0642\u0629 (\u0645\u0633\u062a\u0628\u0639\u062f\u0629)</span><b style="color:#d97706">' + pendingInvoices.length + ' \u0641\u0627\u062a\u0648\u0631\u0629 / ' + fmtMoney(pendingAmount) + '</b></div>' : '') +
         '</div>' +
         '<div class="dr-title">\u0627\u0644\u0645\u0634\u0631\u0648\u0628\u0627\u062a \u0648\u0627\u0644\u0645\u0646\u062a\u062c\u0627\u062a \u0627\u0644\u0645\u0628\u0627\u0639\u0629</div>' +
+        '<div style="color:#6b7280;font-size:12px;margin:-8px 0 12px">\u0627\u0644\u0623\u0633\u0639\u0627\u0631 \u0627\u0644\u0623\u0633\u0627\u0633\u064a\u0629 \u0642\u0628\u0644 \u0627\u0644\u062e\u0635\u0648\u0645\u0627\u062a \u0648\u0627\u0644\u0639\u0645\u0644\u0627\u062a \u2014 \u0627\u0644\u0625\u062c\u0645\u0627\u0644\u064a \u0627\u0644\u0641\u0639\u0644\u064a \u064a\u0636\u0645\u0646 \u0627\u0644\u062e\u0635\u0648\u0645\u0627\u062a</div>' +
         buildDrinkTable(itemsMap) +
         '<div class="dr-title">\u0645\u0631\u062a\u062c\u0639\u0627\u062a \u0627\u0644\u064a\u0648\u0645</div>' +
         buildReturnTable(dayReturns) +
